dc006fa15826daa5a8bbf42b2a21c2656076ca95
Yesh Dattatreya <ydatta@amazon.com>
2024-08-16 14:57:44 -0700

    Simplified content to not limit the training samples

--- Policy_Gradients/main.py
+++ Policy_Gradients/main.py
@@ -2,26 +2,39 @@ import gym, torch, numpy as np, warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 from policy import Policy
 import utils, time
+from torch.optim.lr_scheduler import StepLR, ExponentialLR
 
 def main(policy_filename):
     env = gym.make('LunarLander-v2', new_step_api=True)
+    
+    # Initialize the policy
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
-    envs = [gym.make('LunarLander-v2', new_step_api=True) for i in range(10)]
     policy = Policy(state_dim, action_dim)
-    num_trajectories = 1024
-    time_steps_per_trajectory = 1024
+
+    num_trajectories = 1000
     num_epochs = 200
     optimizer = torch.optim.Adam(params=policy.parameters())
+    # scheduler = ExponentialLR(optimizer, gamma=0.9)
     losses = []
+
     for i in range(num_epochs):
         t0 = time.time()
-        observations, actions, rewards, sum_discounted_rewards = utils.get_samples_parallel(envs, policy, num_samples=num_trajectories, time_steps=time_steps_per_trajectory)
+        
+        # Sample trajectories from current policy
+        observations, actions, rewards, sum_discounted_rewards = utils.get_samples_parallel(
+            env, policy, num_samples=num_trajectories
+        )
         t1 = time.time()
-        policy, loss = utils.train_step(policy, optimizer, observations, actions, sum_discounted_rewards)
+
+        # Train the policy on the samples obtained
+        policy, loss = utils.train_step(policy, optimizer, observations, actions, sum_discounted_rewards, num_samples=num_trajectories)
         t2 = time.time()
+
+        # Bookkeeping
         print(f"{i}: Loss {loss:0.4f}, sampling time {t1 - t0:0.4f}, training time {t2 - t1:0.4f}")
         losses.append(loss)
+        # scheduler.step()
         # save checkpoint after every 10 epochs
         if i % 20 == 0 and i != 0:
             torch.save(policy.state_dict(), f"{policy_filename}_{i}.pt")
@@ -47,24 +60,27 @@ def run(policy_filename, steps):
             print(f"{count}: ========== Environment reset ==========")
 
 def get_samples():
-    envs = [gym.make('LunarLander-v2', new_step_api=True) for i in range(10)] 
+    env = gym.make('LunarLander-v2', new_step_api=True)
     policy = Policy(state_dim=8, action_dim=4)
-    # policy.load_state_dict(torch.load("policy.pt"))
-    # policy.eval()
-    return utils.get_samples_parallel(envs, policy, num_samples=10, time_steps=12)
+    return utils.get_samples(env, policy, num_samples=5)
+
+def get_samples_parallel():
+    # envs = [gym.make('LunarLander-v2', new_step_api=True) for i in range(10)] 
+    env = gym.make('LunarLander-v2', new_step_api=True)
+    policy = Policy(state_dim=8, action_dim=4)
+    return utils.get_samples_parallel(env, policy, num_samples=20)
 
 def get_trajectory():
     env = gym.make('LunarLander-v2', new_step_api=True)
     policy = Policy(state_dim=8, action_dim=4)
     # policy.load_state_dict(torch.load("policy.pt"))
-    # policy.eval()
     return utils.get_trajectory(env, policy, time_steps=12, discount=0.9)
 
 if __name__ == '__main__':
-    policy_filename = "policy_5_100"
-    # losses = main(policy_filename)
-    # print(losses)
-    run(policy_filename, 4000)
-    # o, a, r, dr = get_trajectory()
-    # print(a, r, dr)
+    policy_filename = "checkpoints/policy1"
+    losses = main(policy_filename)
+    print(losses)
+    # run(policy_filename, 3000)
+    # observations, actions, rewards, sum_discounted_rewards = get_samples_parallel()
+    # print(observations.shape, actions.shape, rewards.shape, sum_discounted_rewards.shape)
     
--- Policy_Gradients/utils.py
+++ Policy_Gradients/utils.py
@@ -5,23 +5,22 @@ import numpy as np
 from sklearn.preprocessing import normalize
 
 
-def get_trajectory(env, policy, time_steps, discount=0.9):
+def get_trajectory(env, policy, discount=0.9, sampling="deterministic"):
     observations, actions, rewards, sum_discounted_rewards = [], [], [], [] # State, Action, Reward, Sum of discounted rewards
-    terminated, truncated, count = False, False, 0
+    terminated, truncated = False, False
     observation = env.reset()
-    while (not (terminated or truncated)) and count < time_steps:
+    while (not (terminated or truncated)): # and count < time_steps:
+        observation = torch.tensor(observation, dtype=torch.float32)
         observations.append(observation)
+
+        # Get the action from the logits
         policy.eval()
         with torch.no_grad():
-            logits = policy(torch.tensor(observation, dtype=torch.float32).view(1,-1)) 
-        
-        # Sample an action probabilitistically
-        # action = torch.multinomial(
-        #     F.softmax(logits, dim=1), num_samples=1
-        # ).item()
-
-        # Sample an action deterministically
-        action = torch.argmax(logits).item()
+            logits = policy(observation.view(1,-1))         
+        if sampling == "deterministic":
+            action = torch.argmax(logits).item()
+        else:
+            action = torch.multinomial(F.softmax(logits, dim=1), num_samples=1).item()
 
         # Take a step in the environment
         observation, reward, terminated, truncated, info = env.step(action)
@@ -29,49 +28,35 @@ def get_trajectory(env, policy, time_steps, discount=0.9):
         # Store the results
         actions.append(action)
         rewards.append(reward)
-        if reward >= 100:
-            print(f"Reward {reward} is >= 100")
-        count += 1
         if sum(rewards) < -250:
             break
-    if sum(rewards) > 100:
-        print(f"Sum of reward {sum(rewards)} is larger than 100")
 
     # Get discounted rewards and normalize it to reduce variance
     sum_discounted_rewards = get_discounted_rewards(rewards, discount)
     sum_discounted_rewards = normalize(np.array(sum_discounted_rewards).reshape(1,-1)).reshape(-1)
 
-    # Pad the trajectory so we have the same number of observations and actions for training
-    observations, actions, sum_discounted_rewards = pad_trajectory(observations,actions, sum_discounted_rewards, time_steps)
-    return observations, actions, rewards, sum_discounted_rewards
+    return torch.vstack(observations), torch.tensor(actions), torch.tensor(rewards), torch.tensor(sum_discounted_rewards)
 
-def pad_trajectory(observations, actions, sum_discounted_rewards, time_steps, neutral_action=0):
-    # Pad the trajectory for use in training
-    observations = np.array(observations)
-    observations = np.vstack([observations, np.tile(observations[-1], (time_steps - observations.shape[0], 1))])
-    actions = np.pad(np.array(actions), (0, time_steps - len(actions)), "constant", constant_values=neutral_action)
-    sum_discounted_rewards = np.pad(np.array(sum_discounted_rewards), (0, time_steps - len(sum_discounted_rewards)), "constant", constant_values=0.0)
-    return observations, actions, sum_discounted_rewards
-
-def get_samples(env, policy, num_samples, time_steps, discount=0.9):
+def get_samples(env, policy, num_samples, discount=0.9):
     observations, actions, rewards, sum_discounted_rewards = [], [], [], []
     for i in range(num_samples):
-        o, a, r, dr = get_trajectory(env, policy, time_steps, discount)        
+        o, a, r, dr = get_trajectory(env, policy, discount)        
         observations.append(o)
         actions.append(a)
         rewards.append(r)
         sum_discounted_rewards.append(dr)         
-    return observations, actions, rewards, sum_discounted_rewards
+    return torch.vstack(observations), torch.cat(actions), torch.cat(rewards), torch.cat(sum_discounted_rewards)
 
-def get_samples_parallel(envs, policy, num_samples, time_steps, discount=0.9, num_processes=None):
+def get_samples_parallel(env, policy, num_samples, discount=0.9, num_processes=None):
     observations, actions, rewards, sum_discounted_rewards = [], [], [], []
     if num_processes is None:
         num_processes = multiprocessing.cpu_count()
+    num_processes = min(num_processes, num_samples)
 
     with multiprocessing.Pool(processes=num_processes) as pool:
         results = pool.starmap(
             get_samples,
-            [(env, policy, num_samples//num_processes, time_steps, discount) for env in envs]
+            [(env, policy, num_samples//num_processes, discount) for _ in range(num_samples)]
         )
     for result in results:
         o, a, r, dr = result 
@@ -79,9 +64,10 @@ def get_samples_parallel(envs, policy, num_samples, time_steps, discount=0.9, nu
         actions.append(a)
         rewards.append(r)
         sum_discounted_rewards.append(dr)
-    observations = np.vstack(observations)
-    actions = np.vstack(actions)
-    sum_discounted_rewards = np.vstack(sum_discounted_rewards)
+    observations = torch.vstack(observations)
+    actions = torch.cat(actions)
+    rewards = torch.cat(rewards)
+    sum_discounted_rewards = torch.cat(sum_discounted_rewards)
 
     return observations, actions, rewards, sum_discounted_rewards
 
@@ -94,23 +80,18 @@ def get_discounted_rewards(rewards, discount=0.9):
         sum_of_discounted_rewards.append(np.sum(rewards[i:] * discounts[:n-i]))
     return sum_of_discounted_rewards
 
-def train_step(policy, optimizer, observations, actions, sum_discounted_rewards):
+def train_step(policy, optimizer, observations, actions, sum_discounted_rewards, num_samples):
     if torch.cuda.is_available():
         device = "cuda"
     else:
         device = "cpu"
     policy.to(device)
     policy.train()
-    # observations has dimensions num_trajectories X time_steps X state_dim (which is 8 for lunar lander)
-    N, T, _ = observations.shape
-    observations = torch.tensor(observations).view(-1,observations[0].shape[-1])
     observations = observations.to(device)
     logits = policy(observations)
-    sum_discounted_rewards = torch.tensor(sum_discounted_rewards).view(-1)
     sum_discounted_rewards = sum_discounted_rewards.to(device)
-    actions = torch.tensor(actions).view(-1)
     actions = actions.to(device)
-    loss = (1/N) * (F.cross_entropy(logits, actions, reduction="none")*sum_discounted_rewards).sum()
+    loss = (1/num_samples) * (F.cross_entropy(logits, actions, reduction="none")*sum_discounted_rewards).sum()
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
